61ac32420c5eeb097c8259070965724eb686498a
curamba <athlon_dean@yahoo.com>
2016-06-19 22:43:06 +0800

    Update perfanalysis.php
    -ALL
    -now included display specific grade of a particular student (for teacher/admin user only)
    -now with average grade for each maintopic grades

--- perfanalysis.php
+++ perfanalysis.php
@@ -3,7 +3,6 @@ include_once('ECheader.php');
 /*
 if(!(trim($usertype)=="admin" || trim($usertype)=="teacher")){
   siteRedirectWithAlert("Sorry you are not allowed to use this module! ".$usertype,"index.php");
-
 }
 */
 ?>
@@ -38,16 +37,54 @@ group by t.testsessionid,t.topicid,t.subjid;
 
 */
 $studentID = '';
+if(isset($_POST['studentID'])) $studentID = $_POST['studentID']; 
+$details = "";
+$searchthis="";
+$studentname = "";
+$studentQname = "";
 // deter the user type
 if(trim($usertype)=="admin" || trim($usertype)=="teacher"){
   //display a form to require user the studentID to view grade report
   //(we must provide student masterlist)
-  
-  // make sure that the user had input a valid studentID
-  // else prompt him for re input
-  // redirect to index/mainpage
+  $searchthis = "<form action='#' method='POST'>
+  				Provide Student ID #: <input name='studentID' type='text' id='usersfilter' value ='$studentID' />
+				<input type='submit' name='submit' id='submit' value='Display Grade' />
+			  </form>";
+  if(isset($_POST['studentID'])){
+  	$studentID = $_POST['studentID'];
+	$studentQname = "<div align='right'>Student Name : </div>";
+	$studentname = trim(getfieldvalue(" personalinfo ", "fname", " where id='$studentID' and usertype='student'"));
+	$studentname .= " " . trim(getfieldvalue(" personalinfo ", "mname", " where id='$studentID' and usertype='student'"));
+	$studentname .= " " . trim(getfieldvalue(" personalinfo ", "lname", " where id='$studentID' and usertype='student'"));
+	$studentname =strtoupper($studentname);
+	if(strlen(trim($studentname))<1){
+		$studentname = "<font color = 'red'> Sorry No Record found....</font>";
+		/*
+		$studentname = "
+		<td width='20%' bgcolor='#FFFF00'>$studentQname</td>
+        <td width='40%' bgcolor='#FFFF00'><font color = 'red'> Sorry No Record found....</font></td>
+        ";
+		*/
+	}else {
+		$studentname = "<div align='left'>" . $studentname . "</div>";
+		/*
+		$studentname = "
+		<td width='20%' bgcolor='#FFFF00'>$studentQname</td>
+        <td width='40%' bgcolor='#FFFF00'><font color = 'red'> $studentname </font></td>
+        ";	
+		*/	
+	}
+	
+  }
 }else {  // the current user is a student
 	$studentID = $_SESSION['studentID'];
+	$studentname = ''; //$_SESSION['wholename'];
+	/*
+	$studentname = "
+		<td width='20%' bgcolor='#FFFF00'> </td>
+        <td width='40%' bgcolor='#FFFF00'> </td>
+	";
+	*/
 }
 
 //open studyunits and form the sql statement to populate user display
@@ -56,7 +93,7 @@ if(trim($usertype)=="admin" || trim($usertype)=="teacher"){
 
 
 ////////////////////////////////////////////////////////////////////
-$details = "";
+
 // Create connection
 require("controllers/connection.php");
 global $con;
@@ -79,9 +116,9 @@ if ($result) {
 		$subjid = $row['id'];
 		if($isparent){
 			$maintopic = $row['studyname'];			
-			$subtopic = '';
+			$subtopic = '&nbsp;';
 		}else{
-			$maintopic = '';			
+			$maintopic = '&nbsp;';			
 			$subtopic = $row['studyname'];		
 		}
 		//$studyname = trim(getfieldvalue(" studyunits ", "studyname", " where topicid='$topicid' and isparent='1'"));
@@ -96,38 +133,109 @@ if ($result) {
 		where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='0' 
 		group by t.testsessionid,t.topicid,t.subjid;
 		*/
+		if($maintopic=='&nbsp;'){
+				// get data for PRE-TEST
+				$testtype = 1; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  
+				$PreTestC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			   $PreTestIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			 $PreTestGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
+		
+				// get data for POST-TEST
+				$testtype = 2; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  
+				$PostTestC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			   $PostTestIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			 $PostTestGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
+		
+				// get data for SHORT-QUIZ
+				$testtype = 3; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  		
+				$ShortQuizC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			   $ShortQuizIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			 $ShortQuizGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
+			 
+				// get data for LONG-QUIZ
+				$testtype = 4; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  		
+				$LongQuizC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			   $LongQuizIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			 $LongQuizGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
+		
+				// get data for MAJOR-EXAM
+				$testtype = 5; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  		
+				$MajorC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			   $MajorIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
+			 $MajorGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
+		}else{
+				/*
+				// grade group
+				select if(avg(x.average) IS NULL," ",avg(x.average)) from (
+				SELECT
+				(sum(correct_ans=ansreceived)/sum(point)*50+50) 'average'
+				FROM testhistory t, studyunits s
+				where t.subjid=s.id and t.testsessionid = '1' and t.testtype='0' and t.topicid='3'
+				group by t.testsessionid,t.topicid,t.subjid) x;
+				// points group
+				select if(sum(x.total) IS NULL," ",sum(x.total)) from (
+				SELECT
+				(sum(correct_ans=ansreceived)) 'total'
+				FROM testhistory t, studyunits s
+				where t.subjid=s.id and t.testsessionid = '1' and t.testtype='1' and t.topicid='1'
+				group by t.testsessionid,t.topicid,t.subjid) x;
+				*/
+				$PostTestC = "";
+				$PostTestIN = "";
+				$PostTestGrade =trim(getfieldvalue(" (
+				SELECT
+				(sum(correct_ans=ansreceived)/sum(point)*50+50) 'average'
+				FROM testhistory t, studyunits s
+				where t.subjid=s.id and t.testsessionid = '$studentID' and t.testtype='1' and t.topicid='$topicid'
+				group by t.testsessionid,t.topicid,t.subjid) x", 
+				"if(round(avg(x.average),2) IS NULL,' ',round(avg(x.average),2))",  
+				" "));
+				
+				$PreTestC ="";
+				$PreTestIN="";
+				$PreTestGrade=trim(getfieldvalue(" (
+				SELECT
+				(sum(correct_ans=ansreceived)/sum(point)*50+50) 'average'
+				FROM testhistory t, studyunits s
+				where t.subjid=s.id and t.testsessionid = '$studentID' and t.testtype='2' and t.topicid='$topicid'
+				group by t.testsessionid,t.topicid,t.subjid) x", 
+				"if(round(avg(x.average),2) IS NULL,' ',round(avg(x.average),2))",  
+				" "));
+				
+				$ShortQuizC ="";
+				$ShortQuizIN="";
+				$ShortQuizGrade=trim(getfieldvalue(" (
+				SELECT
+				(sum(correct_ans=ansreceived)/sum(point)*50+50) 'average'
+				FROM testhistory t, studyunits s
+				where t.subjid=s.id and t.testsessionid = '$studentID' and t.testtype='3' and t.topicid='$topicid'
+				group by t.testsessionid,t.topicid,t.subjid) x", 
+				"if(round(avg(x.average),2) IS NULL,' ',round(avg(x.average),2))",  
+				" "));
+				
+				$LongQuizC="";
+				$LongQuizIN="";
+				$LongQuizGrade=trim(getfieldvalue(" (
+				SELECT
+				(sum(correct_ans=ansreceived)/sum(point)*50+50) 'average'
+				FROM testhistory t, studyunits s
+				where t.subjid=s.id and t.testsessionid = '$studentID' and t.testtype='4' and t.topicid='$topicid'
+				group by t.testsessionid,t.topicid,t.subjid) x", 
+				"if(round(avg(x.average),2) IS NULL,' ',round(avg(x.average),2))",  
+				" "));
+				
+				$MajorC="";
+				$MajorIN="";
+				$MajorGrade=trim(getfieldvalue(" (
+				SELECT
+				(sum(correct_ans=ansreceived)/sum(point)*50+50) 'average'
+				FROM testhistory t, studyunits s
+				where t.subjid=s.id and t.testsessionid = '$studentID' and t.testtype='5' and t.topicid='$topicid'
+				group by t.testsessionid,t.topicid,t.subjid) x", 
+				"if(round(avg(x.average),2) IS NULL,' ',round(avg(x.average),2))", 
+				" "));
 
-		// get data for PRE-TEST
-		$testtype = 1; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  
-		$PreTestC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	   $PreTestIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	 $PreTestGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
-
-		// get data for POST-TEST
-		$testtype = 2; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  
-		$PostTestC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	   $PostTestIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	 $PostTestGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
-
-		// get data for SHORT-QUIZ
-		$testtype = 3; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  		
-		$ShortQuizC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	   $ShortQuizIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	 $ShortQuizGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
-	 
-		// get data for LONG-QUIZ
-		$testtype = 4; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  		
-		$LongQuizC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	   $LongQuizIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	 $LongQuizGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
-
-
-		// get data for MAJOR-EXAM
-		$testtype = 5; // 0-study/review only | 1 - pre-test | 2 - post-test 3..... 5-major exam  		
-		$MajorC = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(correct_ans=ansreceived) IS NULL, ' ',sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	   $MajorIN = trim(getfieldvalue(" testhistory t, studyunits s ", "if(sum(point)-sum(correct_ans=ansreceived) IS NULL, ' ',sum(point)-sum(correct_ans=ansreceived))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'"));
-	 $MajorGrade = trim(getfieldvalue(" testhistory t, studyunits s ", "if(round(sum(correct_ans=ansreceived)/sum(point)*50+50,2) IS NULL, ' ',round(sum(correct_ans=ansreceived)/sum(point)*50+50,2))", " where t.subjid=s.id and t.testsessionid = '$studentID' and subjid = '$subjid' and t.testtype='$testtype'")); 
-	 
+		}
 		$bgcolor = "#3399FF";
 		if($ctr%2){
 			$bgcolor = "#CCFFFF";
@@ -178,12 +286,14 @@ $vsql ="SELECT  t.topicid,t.subjid,s.studyname,
 		";
 
 //die ($vsql);
-	
+
+echo $searchthis;	
 ?>
     <table width='120%' border='1'>
       <tr>
-        <td width='20%'>&nbsp;</td>
-        <td width='40%'>&nbsp;</td>
+        <td width='20%' bgcolor="#FFFF00"><?PHP echo $studentQname ?></td>
+        <td width='40%' bgcolor="#FFFF00"><?PHP echo $studentname ?></td>
+        
         <td colspan='3'><div align='center'>Pre-Test</div></td>
         <td colspan='3'><div align='center'>Post-Test</div></td>
         <td colspan='3'><div align='center'>Short-Quiz</div></td>
